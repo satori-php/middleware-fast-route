79a375576f5fa28bccc510d85e9c4377546a4258
Yuriy Davletshin <yuriy.davletshin@gmail.com>
2017-03-01 06:25:36 +0200

    Added fast-route.php

---
+++ src/fast-route.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Satori\Http\Request;
+
+
+$app['middleware.fast_route'] = function (\Generator $next) use ($app) {
+    $app->notify('start_routing');
+    $capsule = yield;
+    $router = $app->{$app['service.fast_route'] ?? 'router'};
+    $route = $router->dispatch(Request\getMethod(), Request\getPath());
+    switch ($route[0]) {
+        case \FastRoute\Dispatcher::FOUND:
+            $capsule['http.status'] = 200;
+            $capsule['action'] = $route[1];
+            $capsule['uri.parameters'] = $route[2];
+            break;
+
+        case \FastRoute\Dispatcher::NOT_FOUND:
+            $capsule['http.status'] = 404;
+            break;
+
+        case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $capsule['http.status'] = 405;
+            break;
+    }
+    $app->notify('finish_routing');
+    $next->send($capsule);
+
+    return $next->getReturn();
+};
