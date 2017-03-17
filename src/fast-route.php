85134112191fc823eabbb7ec37ae869b15c9e9d5
Yuriy Davletshin <yuriy.davletshin@gmail.com>
2017-03-17 13:13:48 +0200

    Updated fast-route.php

--- src/fast-route.php
+++ src/fast-route.php
@@ -8,30 +8,42 @@
 
 declare(strict_types=1);
 
+namespace Satori\Middleware\FastRoute;
+
+use Satori\Application\ApplicationInterface;
 use Satori\Http\Request;
 
-$app['middleware.fast_route'] = function (\Generator $next) use ($app) {
-    $app->notify('start_routing');
-    $capsule = yield;
-    $router = $app->{$app['service.fast_route'] ?? 'router'};
-    $route = $router->dispatch(Request\getMethod(), Request\getPath());
-    switch ($route[0]) {
-        case \FastRoute\Dispatcher::FOUND:
-            $capsule['http.status'] = 200;
-            $capsule['action'] = $route[1];
-            $capsule['uri.parameters'] = $route[2];
-            break;
+/**
+ * Initializes the FastRout middleware.
+ *
+ * @param ApplicationInterface  $app   The application.
+ * @param string                $id    The unique name of the middleware.
+ * @param array<string, string> $names The array with names `['router' => 'router']`.
+ */
+function init(ApplicationInterface $app, string $id, array $names) {
+    $app[$id] = function (\Generator $next) use ($app, $names) {
+        $app->notify('start_routing');
+        $capsule = yield;
+        $router = $app->{$names['router']};
+        $route = $router->dispatch(Request\getMethod(), Request\getPath());
+        switch ($route[0]) {
+            case \FastRoute\Dispatcher::FOUND:
+                $capsule['http.status'] = 200;
+                $capsule['action'] = $route[1];
+                $capsule['uri.parameters'] = $route[2];
+                break;
 
-        case \FastRoute\Dispatcher::NOT_FOUND:
-            $capsule['http.status'] = 404;
-            break;
+            case \FastRoute\Dispatcher::NOT_FOUND:
+                $capsule['http.status'] = 404;
+                break;
 
-        case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-            $capsule['http.status'] = 405;
-            break;
-    }
-    $app->notify('finish_routing');
-    $next->send($capsule);
+            case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $capsule['http.status'] = 405;
+                break;
+        }
+        $app->notify('finish_routing');
+        $next->send($capsule);
 
-    return $next->getReturn();
-};
+        return $next->getReturn();
+    };
+}
